b183c60bf7a33e9551035ea1682d789e39949c58
Bradley Parker <bparker@Bradleys-MacBook-Air.local>
2025-01-19 22:06:44 -0600

    updated for 3rd edition

--- chapter_02/name.py
+++ chapter_02/name.py
@@ -1,6 +1,2 @@
-first_name = "ada"
-last_name = "lovelace"
-full_name = first_name + " " + last_name
-
-message = "Hello, " + full_name.title() + "!"
-print(message)
+name = "ada lovelace"
+print(name.title())
--- chapter_17/python_repos.py
+++ chapter_17/python_repos.py
@@ -1,46 +1,30 @@
 import requests
-import pygal
-from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-# Make an API call, and store the response.
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-r = requests.get(url)
-print("Status code:", r.status_code)
 
-# Store API response in a variable.
+# Make an API call and check the response.
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Convert the response object to a dictionary.
 response_dict = r.json()
-print("Total repositories:", response_dict['total_count'])
+
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
 
-names, plot_dicts = [], []
+print("\nSelected information about each repository:")
 for repo_dict in repo_dicts:
-    names.append(repo_dict['name'])
-    
-    plot_dict = {
-        'value': repo_dict['stargazers_count'],
-        'label': repo_dict['description'],
-        'xlink': repo_dict['html_url'],
-        }
-    plot_dicts.append(plot_dict)
-
-# Make visualization.
-my_style = LS('#333366', base_style=LCS)
-
-my_config = pygal.Config()
-my_config.x_label_rotation = 45
-my_config.show_legend = False
-my_config.title_font_size = 24
-my_config.label_font_size = 14
-my_config.major_label_font_size = 18
-my_config.truncate_label = 15
-my_config.show_y_guides = False
-my_config.width = 1000
-
-chart = pygal.Bar(my_config, style=my_style)
-chart.title = 'Most-Starred Python Projects on GitHub'
-chart.x_labels = names
-
-chart.add('', plot_dicts)
-chart.render_to_file('python_repos.svg')
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
